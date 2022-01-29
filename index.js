f08296e3b9fda85129f59f0a8b203ca19813fbc1
Nelias99 <alexandrosnelli@gmail.com>
2022-01-29 19:34:19 +0200

    client on

--- index.js
+++ index.js
@@ -46,3 +46,13 @@ mongoose.connect(db, {
 // Bring in the Users route
 const auth = require('./auth');
 app.use('/auth', auth);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+}) 
